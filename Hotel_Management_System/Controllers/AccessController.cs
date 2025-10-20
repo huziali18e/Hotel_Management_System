2e71a04297d73410a918df6b73a5fb0bdd8bf29d
Muhammad Huzaifa Ali <muhammadhuzaifaali785@gmail.com>
2025-10-20 10:24:19 +0500

    First Commit

---
+++ Hotel_Management_System/Controllers/AccessController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Hotel_Management_System.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Hotel_Management_System.Controllers
+{
+    public class AccessController : Controller
+    {
+        public IActionResult Login()
+        {
+            ClaimsPrincipal claimuser = HttpContext.User;
+            if(claimuser.Identity.IsAuthenticated)
+                {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login( VMLogin vMLogin)
+        {
+            if (vMLogin.Email == "Admin12@gmail.com" && vMLogin.Password == "123")
+            {
+                List<Claim> claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, vMLogin.Email),
+                    new Claim("OtherProperties", "Example Role")
+                };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(
+                    claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                AuthenticationProperties Properties = new AuthenticationProperties
+                {
+                    AllowRefresh = true,
+                };
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity),
+                    Properties);
+                return RedirectToAction("Index", "Home");
+            }
+            ViewData["Message"] = "Invalid Credentials, Please try again.";
+            return View();
+        }
+    }
+}
---
+++ Hotel_Management_System/Models/VMLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace Hotel_Management_System.Models
+{
+    public class VMLogin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
